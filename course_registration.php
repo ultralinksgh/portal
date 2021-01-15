64be1f9fa934b19a5cbbd25516dd8f2e7735f5f9
ultralinksgh <kdarkohatobrah@gmail.com>
2021-01-15 15:27:25 +0000

    updated sections

---
+++ course_registration.php
@@ -0,0 +1,11 @@
+<?php require 'sections/header.php'; ?>
+<div class="pcoded-main-container">
+    <div class="pcoded-wrapper">
+        <?php 
+                    require 'sections/sidebar.php'; 
+                    require 'sections/pageheader.php'; 
+                    ?>
+    </div>
+</div>
+
+<?php require 'sections/footer.php'; ?>
---
+++ sections/pageheader.php
@@ -0,0 +1,42 @@
+<div class="pcoded-content">
+    <!-- Page-header start -->
+    <div class="page-header">
+        <div class="page-block">
+            <div class="row align-items-center">
+                <div class="col-md-8">
+                    <div class="page-header-title">
+                        <h5 class="m-b-10">Course Registration</h5>
+                        <p class="m-b-0">Welcome to CCOD Blended Learning</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="index.html"> <i class="fa fa-home"></i> </a>
+                        </li>
+                        <li class="breadcrumb-item"><a href="#!">Dashboard</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page-header end -->
+    <div class="pcoded-inner-content">
+        <!-- Main-body start -->
+        <div class="main-body">
+            <div class="page-wrapper">
+                <!-- Page-body start -->
+                <div class="page-body">
+                    <div class="row">
+
+
+
+                    </div>
+                </div>
+                <!-- Page-body end -->
+            </div>
+            <div id="styleSelector"> </div>
+        </div>
+    </div>
+</div>
